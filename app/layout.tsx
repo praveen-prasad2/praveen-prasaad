fd205296a661cf7c9c4066e663620b679a666c69
Praveen Prasad <praveenprasad2510@gmail.com>
2024-10-25 15:00:54 +0530

    Updated Website design

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { Toaster } from "react-hot-toast";
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
-  title: "Praveen Prasad- Top Software Developer & Founder of Duoph Technologies in Kerala",
-  description: "Explore the portfolio of Hadi Razal, an accomplished software developer and founder of Duoph Technologies. Specializing in full-stack development, modern web technologies, and innovative solutions for businesses worldwide.",
-  keywords: "Praveen Prasad, Software Developer, Full-Stack Developer, Kottayam, Kerala, Best Software Developer in Kerala, Top Developer in Kerala, Duoph Technologies, Web Development, JavaScript, React, Node.js, Next.js, Firebase, Supabase, MySQL, TypeScript, Portfolio, Innovative Solutions, Tech Expert"
+  title: "Praveen Prasad - Top Software Developer & Founder of Duoph Technologies in Kerala",
+  description: "Explore the portfolio of Praveen Prasad, an accomplished software developer and founder of Duoph Technologies. Specializing in full-stack development, modern web technologies, and innovative solutions for businesses worldwide.",
+  keywords: "Praveen Prasad, Software Developer, Full-Stack Developer, Ponnai, Kerala, Best Software Developer in Kerala, Top Developer in Kerala, Duoph Technologies, Web Development, JavaScript, React, Node.js, Next.js, Firebase, Supabase, MySQL, TypeScript, Portfolio, Innovative Solutions, Tech Expert"
 };
 
 
